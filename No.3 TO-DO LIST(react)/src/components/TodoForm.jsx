2f52fb05145c36060d929366614d7e69fe76476b
k0nghaa <k0nghaalee@gmail.com>
2025-09-21 17:49:17 +0900

    feat: Todo 추가 및 토글 기능 구현
    - uuid 라이브러리 설치

--- No.3 TO-DO LIST(react)/src/components/TodoForm.jsx
+++ No.3 TO-DO LIST(react)/src/components/TodoForm.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const TodoForm = () => {
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value);
+
+    setValue("");
+  };
+
   return (
-    <form className="TodoForm">
+    <form className="TodoForm" onSubmit={handleSubmit}>
       <input
         type="text"
         className="todo-input"
+        value={value}
         placeholder="할일을 입력하세요"
+        onChange={(e) => setValue(e.target.value)}
       />
       <button type="submit" className="todo-btn">
         + 추가
