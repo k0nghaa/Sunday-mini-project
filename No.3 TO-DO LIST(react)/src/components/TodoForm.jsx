8ad5ff58d3398c90fd1aa5f7b2e54e6ba2615093
k0nghaa <k0nghaalee@gmail.com>
2025-09-21 16:50:43 +0900

    feat: TodoForm component UI 구현

--- No.3 TO-DO LIST(react)/src/components/TodoForm.jsx
+++ No.3 TO-DO LIST(react)/src/components/TodoForm.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const TodoForm = () => {
-  return <div>TodoForm</div>;
+  return (
+    <form className="TodoForm">
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="할일을 입력하세요"
+      />
+      <button type="submit" className="todo-btn">
+        + 추가
+      </button>
+    </form>
+  );
 };
