e75059196691873aa766377750ab633e8ce161a1
k0nghaa <k0nghaalee@gmail.com>
2025-09-28 17:51:37 +0900

    design: ResultList.jsx margin 등 스타일 변경

--- No-4-RANDOM-PICKER/src/components/ResultList.jsx
+++ No-4-RANDOM-PICKER/src/components/ResultList.jsx
@@ -6,14 +6,17 @@ const ResultList = ({ results, searchKeyword }) => {
       <h2 className="text-lg font-semibold mb-4">검색 결과</h2>
 
       {!isSearching && (
-        <p className="text-gray-500 mb-5">
-          검색어를 입력하면 결과가 여기에 표시됩니다.
-        </p>
+        <div>
+          <p className="text-gray-500 mb-8 border-b border-gray-200">
+            검색어를 입력하면 결과가 여기에 표시됩니다.
+          </p>
+          <p className="text-lg font-semibold mb-2">전체 리스트</p>
+        </div>
       )}
       <div>
         {(isSearching ? results : results).map((item) => (
-          <div key={item.id}>
-            <h2>{item.title}</h2>
+          <div className="mb-2 border-b border-gray-400" key={item.id}>
+            <h2 className="text-lg font-semibold mb-2">{item.title}</h2>
             <p>{item.description}</p>
           </div>
         ))}
