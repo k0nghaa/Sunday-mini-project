20e6dd5f12e4e63275d742b7f17021dfbbed2a0a
k0nghaa <k0nghaalee@gmail.com>
2025-09-28 15:35:22 +0900

    chore: 프로그램 초기 세팅 - Tailwind CSS

--- No-4-RANDOM-PICKER/src/App.jsx
+++ No-4-RANDOM-PICKER/src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
 
 function App() {
-  return <>App</>;
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind 세팅 완료! 🚀
+      </h1>
+    </div>
+  );
 }
 
 export default App;
