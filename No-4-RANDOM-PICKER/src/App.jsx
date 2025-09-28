e2ae4c42c69ca8eb923771e72651c3af9ffb02f8
k0nghaa <k0nghaalee@gmail.com>
2025-09-28 16:28:24 +0900

    design: App.jsx 전체 레이아웃 뼈대 구성

--- No-4-RANDOM-PICKER/src/App.jsx
+++ No-4-RANDOM-PICKER/src/App.jsx
@@ -3,7 +3,19 @@ import ResultList from './components/ResultList';
 import RandomPicker from './components/RandomPicker';
 
 function App() {
-  return <></>;
+  return (
+    <div className="flex flex-col h-screen bg-gray-50">
+      <SearchBar />
+      <main className="flex flex-1">
+        <div className="flex-1 p-4 overflow-y-auto">
+          <ResultList />
+        </div>
+        <aside className="w-1/3 p-4 bg-gray-100 border-l">
+          <RandomPicker />
+        </aside>
+      </main>
+    </div>
+  );
 }
 
 export default App;
