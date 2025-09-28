66e6468258abfe4130900a8e8fce0e431a4f2679
k0nghaa <k0nghaalee@gmail.com>
2025-09-28 16:45:41 +0900

    design: RandomPicker.jsx UI 구성

--- No-4-RANDOM-PICKER/src/components/RandomPicker.jsx
+++ No-4-RANDOM-PICKER/src/components/RandomPicker.jsx
@@ -1,5 +1,13 @@
 const RandomPicker = () => {
-  return <div>RandomPicker</div>;
+  return (
+    <div className="flex flex-col itmes-center justify-center h-full">
+      <h2 className="text-lg font-semibold mb-4">랜덤 추천</h2>
+      <button className="bg-blue-500 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-600">
+        돌리기
+      </button>
+      <p className="mt-4 text-gray-600">결과가 여기에 표시됨</p>
+    </div>
+  );
 };
 
 export default RandomPicker;
