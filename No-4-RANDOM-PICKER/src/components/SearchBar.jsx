4fa54a2a7593b0283b91dccf8066d028580bfdf1
k0nghaa <k0nghaalee@gmail.com>
2025-09-28 16:32:40 +0900

    design: SearchBar.jsx UI 구성

--- No-4-RANDOM-PICKER/src/components/SearchBar.jsx
+++ No-4-RANDOM-PICKER/src/components/SearchBar.jsx
@@ -1,5 +1,20 @@
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div>
+      <header className="p-4 shadow bg-white">
+        <div className="flex max-w-xl mx-auto">
+          <input
+            type="text"
+            placeholder="키워드를 입력하세요 (예: 가족, 여행, 판타지)"
+            className="flex-1 border rounded-l px-4 py-2 focus:outline-none"
+          />
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-r">
+            검색
+          </button>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default SearchBar;
